c03e2dc8357d87e6a1fae7d915d82d7e737f5be0
Brendan Grubb <brendangrubb@gmail.com>
2017-01-27 08:40:19 -0800

    add and test initial files

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// Business Logic
+var works = console.log("This Part Works!");
+var worksToo = console.log("This Part Works, Too!!");
+
+
+
+works;
+
+
+
+
+
+
+// UI Logic
+$(document).ready(function() {
+
+
+  worksToo;
+
+
+
+
+});
