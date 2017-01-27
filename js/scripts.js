d8dbd2000bdf780ba83e6f9146db7d4cd4e6fc8c
Brendan Grubb <brendangrubb@gmail.com>
2017-01-27 13:24:47 -0800

    link contact submit form to confirmation page

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ function Topping(name, type, upcharge) {
   this.upcharge = upcharge;
 };
 
-function Customer(name, number, address) {
+function Customer(name, number) {
   this.name = name;
   this.number = number;
   this.address = [];
@@ -38,15 +38,17 @@ function Address(street, city, state) {
 
 // UI Logic
 $(document).ready(function() {
+  // event.preventDefault();
 
-
-
-// BUTTON to choose custom or premade and enter choose size
+// BUTTONS
+// button to choose custom or premade and enter choose size
   $("#custom-pizza-button").click(function() {
     $("#welcome-page").hide();
     $("#custom-size-page").fadeIn(1000);
   });
-// BUTTONS to choose size and enter select toppings
+// buttonS to choose size and enter select toppings
+  var size = $();
+
   $("#small-custom-button").click(function() {
     $("#custom-size-page").hide();
     $("#custom-choose-topping-page").fadeIn(1500);
@@ -59,20 +61,41 @@ $(document).ready(function() {
     $("#custom-size-page").hide();
     $("#custom-choose-topping-page").fadeIn(1500);
   });
-// BUTTON to select toppings and enter review order
+// button to select toppings and enter review order
   $("#choose-toppings-custom-button").click(function() {
     $("#custom-choose-topping-page").hide();
     $("#custom-order-review-page").fadeIn(1500);
   });
-// BUTTON to review order and enter customer contact
+// button to review order and enter customer contact
   $("#enter-delivery-information-button").click(function() {
+
     $("#custom-order-review-page").hide();
     $("#customer-contact-page").fadeIn(1500);
   });
-// BUTTON to enter customer contact and reach final confirmation page
+// button to enter customer contact and reach final confirmation page
   $("#go-to-confirmation-page").click(function() {
+    var nameInput = $("#name-input").val();
+    var phoneInput = $("#phone-input").val();
+    var streetInput = $("#street-input").val();
+    var cityInput = $("#city-input").val();
+    var stateInput = $("#state-input").val();
+    var newCustomer = new Customer(nameInput, phoneInput);
+    var newAddress = new Address(streetInput, cityInput, stateInput);
+    console.log(newAddress);
+    newCustomer.address.push(newAddress);
+    console.log(newCustomer);
+
+    $("#final-confirmation-name").text(newCustomer.name);
+    $("#final-confirmation-address").text(newAddress.street);
+    $("#final-confirmation-city").text(newAddress.city);
+    $("#final-confirmation-state").text(newAddress.state);
     $("#customer-contact-page").hide();
     $("#final-confirmation-page").fadeIn(1500);
+  });
+// button back to main page
+  $("#back-to-welcome-page").click(function() {
+    $("#final-confirmation-page").hide();
+    $("#welcome-page").fadeIn(1500);
   });
 
 });
