dc2d9d6257e362d0417325115ac2d0077dfbd801
Brendan Grubb <brendangrubb@gmail.com>
2017-01-27 09:59:38 -0800

    link size buttons to toppings page

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,27 @@ var worksToo = console.log("This Part Works, Too!!");
 // UI Logic
 $(document).ready(function() {
 
+
+
+// BUTTON to choose custom or premade
   $("#custom-pizza-button").click(function() {
     $("#welcome-page").hide();
-    $("#custom-size-page").fadeIn(1500);
-  })
-
+    $("#custom-size-page").fadeIn(1000);
+  });
+// BUTTONS to choose size
+  $("#small-custom-button").click(function() {
+    $("#custom-size-page").hide();
+    $("#custom-choose-topping-page").fadeIn(1500);
+  });
+  $("#medium-custom-button").click(function() {
+    $("#custom-size-page").hide();
+    $("#custom-choose-topping-page").fadeIn(1500);
+  });
+  $("#large-custom-button").click(function() {
+    $("#custom-size-page").hide();
+    $("#custom-choose-topping-page").fadeIn(1500);
+  });
+// BUTTON to select toppings
 
 
 
