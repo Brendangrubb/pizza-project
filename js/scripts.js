1a7f8be45213566c4bce2e5c45021967e11d241d
Brendan Grubb <brendangrubb@gmail.com>
2017-01-27 16:50:02 -0800

    set cost to add pizza base price with topping upcharge

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
 // Business Logic
+var newOrder = new Order();
+var newCustomer;
+var customerToppings = [];
 
 // CONSTRUCTORS
 function Order(size) {
   this.size = "";
   this.toppings = [];
-  this.cost = [];
-};
-
-function Topping(name, type, upcharge) {
-  this.name = name;
-  this.type = type;
-  this.upcharge = upcharge;
+  this.cost = 0;
 };
 
 function Customer(name, phone) {
@@ -25,21 +22,25 @@ function Address(street, city, state) {
   this.state = state;
 };
 
+// PROTOYPE
+Order.prototype.costCalculator = function() {
+  for (i = 0; i < this.toppings.length; i +=1) {
+    this.cost += 1;
+  }
+
+};
 
 
-
-
-
-
-
+// this.toppings.forEach(function() {
+//   this.cost += 1;
+//   console.log(this.cost);
+// })
 
 // UI Logic
 $(document).ready(function() {
   // event.preventDefault();
 
-    var newOrder = new Order();
-    var newCustomer;
-    var customerToppings = [];
+
 
 
 // BUTTONS
@@ -48,32 +49,36 @@ $(document).ready(function() {
     $("#welcome-page").hide();
     $("#custom-size-page").fadeIn(1000);
   });
+
 // buttonS to choose size and enter select toppings
   $("#small-custom-button").click(function() {
     newOrder.size = ("small");
     $("#order-review-size").text(newOrder.size);
     $("#final-confirmation-size").text(newOrder.size);
+    newOrder.cost = 12;
     $("#custom-size-page").hide();
     $("#custom-choose-topping-page").fadeIn(1500);
-    console.log(newOrder);
-
   });
+
   $("#medium-custom-button").click(function() {
     newOrder.size = ("medium");
     $("#order-review-size").text(newOrder.size);
     $("#final-confirmation-size").text(newOrder.size);
+    newOrder.cost = 15;
     $("#custom-size-page").hide();
     $("#custom-choose-topping-page").fadeIn(1500);
-    console.log(newOrder);
 
   });
+
   $("#large-custom-button").click(function() {
     newOrder.size = ("large");
     $("#order-review-size").text(newOrder.size);
     $("#final-confirmation-size").text(newOrder.size);
+    newOrder.cost = 18;
     $("#custom-size-page").hide();
     $("#custom-choose-topping-page").fadeIn(1500);
   });
+
 // button to select toppings and enter review order
   $("#topping-form").submit(function() {
     event.preventDefault();
@@ -81,16 +86,21 @@ $(document).ready(function() {
       var chosenToppings = $(this).val();
       newOrder.toppings.push(chosenToppings);
     });
+    newOrder.costCalculator();
+    $("#order-review-cost").text(newOrder.cost);
+    $("#final-confirmation-total").text(newOrder.cost);
     $("#order-review-toppings").text(newOrder.toppings);
     $("#final-confirmation-toppings").text(newOrder.toppings);
     $("#custom-choose-topping-page").hide();
     $("#custom-order-review-page").fadeIn(1500);
   });
+
 // button to review order and enter customer contact
   $("#enter-delivery-information-button").click(function() {
     $("#custom-order-review-page").hide();
     $("#customer-contact-page").fadeIn(1500);
   });
+
 // button to enter customer contact and reach final confirmation page
   $("#go-to-confirmation-page").click(function() {
     var nameInput = $("#name-input").val();
